c82dcfaa30dc7571dcf28ef701ea9753042cd6ba
smiller46 <79584815+smiller46@users.noreply.github.com>
2021-11-03 10:43:14 -0400

    Create 01_assignment.py

---
+++ 01_assignment.py
@@ -0,0 +1,40 @@
+# steven
+# Assignment Examples
+
+# you can assign "values" to "variables" by using an equals (right side goes into left side)
+x = 5
+
+# when Python reades a variable name, it replaces it with the variable's stored value
+x = 5
+
+# there are four diffrent primitive datatypes
+# integers: any WHOLE number, positive or negative
+age = 16
+# Float: any number with a decimal, positive or negative
+grade = 98.6
+# string: humen-readable characters
+name = "steven"
+# number in a string are not numbers, they are letters
+favoriteNumber = "69"
+# Boolean: True or False
+# True is any value that is not False or empty
+isSmart = True
+
+# you can output to the console by using "print()"
+print(age)
+print(grade)
+print(name)
+print(favoriteNumber)
+print(isSmart)
+
+# can concatenate values together
+print("My name " + name)
+# you can use functions to convert data types
+print("and my age is" + str(age))
+# dont forget if you want to convert a value permanetly you must assign the converted value to a variable
+age = str(age)
+# you can convert back and forth with int(), str(), bool(), and float()
+print(int(age))
+print(float(age))
+print(bool(age))
+
